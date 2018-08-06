a6eb5aceb0031ac90d55f57d4de72a2cefb8eee0
Yury Ignatev <yurabuy@yandex.ru>
2018-08-06 20:50:38 +0300

    clean

--- calm/4-apps/src/calm/solid/a/Types.scala
+++
@@ -1,9 +0,0 @@
-package calm.solid
-
-import wvlet.surface.tag.@@
-
-object Types {
-  type Login = String@@LoginTag
-  type Password = String@@PasswordTag
-  type SessionId = String@@SessionIdTag
-}
